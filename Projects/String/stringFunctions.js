9123d11e48bb9bbcf4a5d68cc837be3f87d113d3
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-11-04 09:43:10 +0000

    Search and replace

--- Projects/String/stringFunctions.js
+++ Projects/String/stringFunctions.js
@@ -15,3 +15,19 @@ console.log("Original String: ", str);
 console.log("Substring: ", substring);
 console.log("New String: ", newstr);
 console.log("LowerCase: ", lowercase);
+
+//Search and Replace
+const originalStory = `The quick brown fox jumps over the lazy dog
+                        The quick brown fox is know for its agility`
+
+console.log(originalStory);
+
+//perform the search and replace
+
+//changes all matches (//g)
+const manipulatedstory = originalStory.replace(/quick/g, "slow");
+//Changes just the first match
+const manipulatedstory1 = originalStory.replace("quick", "slow");
+
+console.log(manipulatedstory);
+console.log(manipulatedstory1);
