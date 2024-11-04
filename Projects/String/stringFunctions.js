2d4bbb714d4c291511fe0b0803abe671d4e208a3
Bárbara Candido de Souza <barbara.candido.souza@gmail.com>
2024-11-04 09:31:40 +0000

    Update stringFunctions.js
    String Handling

---
+++ Projects/String/stringFunctions.js
@@ -0,0 +1,17 @@
+//Start by manipulating a simple string
+
+let str = "Hello World";
+
+//extact a substring from the string
+let substring = str.substring(0, 5);
+
+//replace a word
+let newstr = str.replace("World", "Universe");
+
+//make the str lowercase
+let lowercase = str.toLowerCase();
+
+console.log("Original String: ", str);
+console.log("Substring: ", substring);
+console.log("New String: ", newstr);
+console.log("LowerCase: ", lowercase);
